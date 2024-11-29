67b81dd92f8f97ea6a091210b0fecdccf49d64a3
Liya2024mhc <liya@myheartcreative.com>
2024-11-29 08:58:49 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(null > 0) //f
+// console.log(null == 0) //f
+// console.log(null >= 0) //t
+// == and comparison(> < >=, <=)work differently. 
+//when comparison converts null to a numbers may be 0 ie why (null>=0) ----> (0>=0)
+
+
+// console.log("2" == 2)  ---> here only the numbers are comparing
+// console.log("2" === 2) ---> here the numbers and type also comparing
