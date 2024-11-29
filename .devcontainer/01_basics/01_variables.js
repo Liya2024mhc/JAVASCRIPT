f6edb39fa0973bb3d13617f18a2910ea41491df4
Liya2024mhc <liya@myheartcreative.com>
2024-11-29 05:48:51 +0000

    variable and datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 2001
+let accountEmail =  "myheartcreative@gmail.com"
+var accountPassword = "12345"
+accountCity = "Banglore"
+let accountState
+
+// accountId = 2002
+accountEmail =  "liya@gmail.com"
+accountPassword = "112233"
+accountCity = "Kottayam"
+// console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// alert(hello)  it will compile because we are using node not the browser
+
+alert("hello")
