63046984cc9acb0ae1b4743bca7fbd02d82e5544
Liya2024mhc <liya@myheartcreative.com>
2024-11-29 07:13:52 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 33
+
+// "33 " => string--> number => 33
+// "33abc" => NaN
+// true => 1
+
+let loggedIn = 0
+let booleanLoginIn = Boolean(loggedIn)
+console.log(booleanLoginIn)
+
+let number = 28
+let numberString = String(number)
+console.log(number)
+console.log(typeof numberString)
