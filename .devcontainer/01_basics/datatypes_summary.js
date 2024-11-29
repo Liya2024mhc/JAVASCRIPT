e62970fb891ffae09be0dcbcacdf378c46550371
Liya2024mhc <liya@myheartcreative.com>
2024-11-29 09:58:58 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,5 @@
+// primitive : String(S), number(N), boolean(B), null(O), undefined(U), symbol(S),bigint(B)
+//reference : non-primitive ----> array(O), object(O), functions(F)
+
+const num= 1234567891011121314151617181920n;
+console.log(typeof num)
