5b0367c06d78001b7df08de702edeb8e6cc87029
Liya2024mhc <liya@myheartcreative.com>
2024-12-03 11:02:11 +0000

    string

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const fullName= "Liya K Joseph"
+const companyName = "myheartcreative"
+console.log(`My name is ${fullName}. I am working in ${companyName}`)
+
+const n = new String('Leahmaria')
+console.log(n.__proto__)
+console.log(n.length)
+console.log(n.toUpperCase())
+console.log(n.charAt(3))
+console.log(n.indexOf('m'))
